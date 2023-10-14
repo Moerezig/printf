4b08d592286c89aa33462f1bd715ebbbb45048ec
Moerezig <mohammedrzq53@gmail.com>
2023-10-14 13:27:45 -0700

    lkm

--- main.h
+++ main.h
@@ -1,14 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
-#define buffer_size 1024
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-
-int _printf(const char *format, ...);
-int _putchar(char c);
-=======
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -21,5 +12,4 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_number(int n);
 
->>>>>>> d1a5228d767a335ac95acce8926b18a9897d8bab
 #endif
