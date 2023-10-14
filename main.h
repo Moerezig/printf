8c6c05d7a8e74a7412e4c63802a85cc0e8498ecc
Moerezig <mohammedrzq53@gmail.com>
2023-10-14 11:05:24 -0700

    lmn

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define buffer_size 1024
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
