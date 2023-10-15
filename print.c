231dc9ea9fc7c654d5403cb1d53897e2094788c8
Moerezig <mohammedrzq53@gmail.com>
2023-10-15 00:00:12 -0700

    lk

--- print.c
+++ print.c
@@ -6,67 +6,67 @@
  *
  * Return: The number of characters printed (excluding the null byte)
  */
-
-	int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-    	va_list args;
+	va_list args;
 	int count = 0;
 	char *str_arg;
 	int int_arg;
 	char char_arg;
 
-
 	va_start(args, format);
 
 	while (*format)
 	{
-	if (*format == '%')
-	{
-	format++;
-
-	switch (*format)
-	{
-		case 'c':
-			char_arg = va_arg(args, int);
-			count += _putchar(char_arg);
-			break;
-		case 's':
-			str_arg = va_arg(args, char *);
-			if (str_arg == NULL)
-			str_arg = "(null)";
-			while (*str_arg)
+		if (*format == '%')
 		{
-			count += _putchar(*str_arg);
-			str_arg++;
-                }
-			break;
-		case 'd':
-		case 'i':
-			int_arg = va_arg(args, int);
-			count += print_number(int_arg);
-			break;
-		case '%':
-			count += _putchar('%');
-			break;
-		default:
-			count += _putchar('%');
+			format++;
+
+			switch (*format)
+			{
+			case 'c':
+				char_arg = va_arg(args, int);
+				count += _putchar(char_arg);
+				break;
+			case 's':
+				str_arg = va_arg(args, char *);
+				if (str_arg == NULL)
+					str_arg = "(null)";
+				while (*str_arg)
+				{
+					count += _putchar(*str_arg);
+					str_arg++;
+				}
+				break;
+			case 'd':
+			case 'i':
+				int_arg = va_arg(args, int);
+				// **Correction:** Check for negative numbers and print the sign.
+				if (int_arg < 0) {
+					count += _putchar('-');
+					int_arg *= -1;
+				}
+				count += print_number(int_arg);
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+			default:
+				count += _putchar('%');
+				count += _putchar(*format);
+				break;
+			}
+		}
+		else
+		{
 			count += _putchar(*format);
-			break;
 		}
+
+		format++;
 	}
-
-	else
-        	{
-
-		count += _putchar(*format);
-		
-		}
-
-
-	format++;
-    }
 
 	va_end(args);
 
-	return count;
+	return (count);
 }
+
