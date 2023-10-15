6c3686320da10aa576b68ed0b732bedd500ac98d
Moerezig <mohammedrzq53@gmail.com>
2023-10-15 01:49:09 -0700

    lk

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_number(int n);
+
+#endif
